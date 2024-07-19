656da84a7418adf9ecd4798c6ea8847562ea9594
Guillaume Ribas <guillaume.ribas@hotmail.fr>
2024-07-19 17:25:54 +0200

    bug sign up & sign in

--- app/(auth)/sign-in.jsx
+++ app/(auth)/sign-in.jsx
@@ -1,21 +1,41 @@
-import { View, Text, ScrollView, Image } from 'react-native'
+import { View, Text, ScrollView, Image, Alert } from 'react-native'
 import React, { useState } from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { images } from '../../constants'
 import FormField from '../../components/FormField'
 import CustomButton from '../../components/CustomButton'
 import { Link } from 'expo-router'
+import { getCurrentUser, signIn } from '../../lib/appwrite'
+import { useGlobalContext } from '../../context/GlobalProvider'
 
 const SignIn = () => {
-  const[form, setForm] = useState({
+  const {setUser, setIsLogged} = useGlobalContext()
+  const [isSubmitting, setIsSubmitting] = useState(false)
+  const [form, setForm] = useState({
     email: '',
     password: ''
   })
 
-  const [isSubmitting, setIsSubmitting] = useState(false)
+  const submit = async () => {
+    if(!form.email || !form.password) {
+      Alert.alert('Error', 'All fields are required')
+    }
 
-  const submit = () => {
+    setIsSubmitting(true)
+    try {
+      await signIn(form.email, form.password)
+      const result = await getCurrentUser()
+      setUser(result)
+      setIsLogged(true)
 
+      //set it to global state
+
+      router.replace('/home')
+    } catch (error) {
+      Alert.alert('Error', error.message)
+    } finally {
+      setIsSubmitting(false)      
+    }
   }
 
   return (
